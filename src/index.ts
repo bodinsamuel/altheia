7241bc5c1fe3014547b9f6857c3a5c9d9442963b
Samuel Bodin <samuel.bodin@algolia.com>
2020-02-28 18:19:06 +0100

    also export validators

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,7 @@ export * from './types/instance';
 export * from './types/lang';
 export * from './types/tests';
 export * from './types/validator';
+
+export * from './validators';
+
 export default Altheia;
